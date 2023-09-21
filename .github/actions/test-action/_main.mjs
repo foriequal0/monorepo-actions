801efa7d47fcca81adc69c60138dc926b723b287
SeongChan Lee <foriequal@gmail.com>
2023-09-21 17:38:33 +0900

    asdf

--- .github/actions/test-action/_main.mjs
+++ .github/actions/test-action/_main.mjs
@@ -1,8 +1,9 @@
 import * as child_process from "node:child_process";
 import * as path from "node:path";
 
-console.log(path.resolve());
-child_process.exec("pwd; env; echo main; npm version;", (error, stdout, stderr) => {
+console.log(import.meta.url);
+
+child_process.exec("pwd; env; echo main; npm version; cat $GITHUB_EVENT_PATH", (error, stdout, stderr) => {
   if (error) {
     console.error(`exec error: ${error}`);
     return;
