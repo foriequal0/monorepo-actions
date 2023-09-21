3c4442fbcbbc681521bb22f768cf43a83b0a1f85
SeongChan Lee <foriequal@gmail.com>
2023-09-21 17:29:17 +0900

    asdf

--- .github/actions/test-action/_main.mjs
+++ .github/actions/test-action/_main.mjs
@@ -1,8 +1,10 @@
-import * as process from "node:process";
 import * as child_process from "node:child_process";
-import * as util from "node:util";
 
-console.log(process.env);
-const exec = util.promisify(child_process.exec);
-await exec("echo main");
-await exec("npm version")
+child_process.exec("env; echo main; npm version;", (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  console.error(`stderr: ${stderr}`);
+});
